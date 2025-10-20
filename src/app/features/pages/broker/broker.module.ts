76bbe43a690fb7a86e798a4a041e43326e07c9ad
steffany <steffany.chavez@cirries.com>
2025-10-20 14:47:45 -0400

    Added broker module

---
+++ src/app/features/pages/broker/broker.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BrokerRoutingModule } from './broker-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrokerRoutingModule
+  ]
+})
+export class BrokerModule { }
