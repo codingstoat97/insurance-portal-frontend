f57b91b4550aff16a9998b4a93e5893161e8de27
steffany <steffany.chavez@cirries.com>
2025-10-20 14:48:06 -0400

    Cleaned up code on quote-stepper

--- src/app/features/pages/quotes/pages/quote-stepper/quote-stepper.component.ts
+++ src/app/features/pages/quotes/pages/quote-stepper/quote-stepper.component.ts
@@ -12,19 +12,39 @@ export class QuoteStepperComponent {
 
   @ViewChild('stepper') stepper!: MatStepper;
 
+  clientData: any;
+  vehicleData: any;
+
   clientDone = false;
   vehicleDone = false;
 
   onClientSubmitted(client: Client) {
+    console.log(client);
+    this.clientData = client;
     this.clientDone = true;
     this.stepper.next();
   }
 
   onVehicleSubmitted(vehicle: Vehicle) {
     console.log(vehicle);
-    
+    this.vehicleData = vehicle;
     this.vehicleDone = true;
+    this.buildParams();
     this.stepper.next();
+  }
+
+  sendForm(): void {
+    const params = this.buildParams();
+    //send to backend
+  }
+
+  buildParams() {
+    const payload = {
+      ...this.vehicleData,
+      ...this.clientData,
+    };
+    console.log(JSON.stringify(payload));
+    return payload;
   }
 
   onCancelled() {
