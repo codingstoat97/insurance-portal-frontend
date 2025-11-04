0ef5cd7aa5ca99147acb93757f6de4bd73535208
steffany <steffany.chavez@cirries.com>
2025-11-03 21:52:50 -0400

    Added request paths util

---
+++ src/app/shared/utils/request-paths.util.ts
@@ -0,0 +1,26 @@
+export const vehiclePath = 'vehicleCatalog';
+export const vehicleList = 'vehicleCatalog/list';
+export const vehicleAdd = 'vehicleCatalog/add';
+export const vehicleUpdate = 'vehicleCatalog/edit';
+export const vehicleGetByID = 'vehicleCatalog/getById';
+export const vehicleClassificationList = 'vehicleCatalog/vehicleClassification';
+
+export const regionPath = 'regionals';
+export const regionList = 'regionals/list'; 
+export const regionAdd = 'regionals/add';
+export const regionUpdate = 'regionals/edit';
+export const regionDelete = 'regionals/delete';
+export const regionGetByID = 'regionals/getById';
+
+export const benefitPath = 'benefits';
+export const benefitList = 'benefits/list';
+
+export const planPath = 'plans';
+export const planList = 'plans/list'
+
+export const insurancePath = 'insurances'
+export const insuranceList = 'insurances/list'
+export const insuranceAdd = 'insurances/add';
+export const insuranceUpdate = 'insurances/edit';
+export const insuranceDelete = 'insurances/delete';
+export const insuranceGetByID = 'insurances/getById';
