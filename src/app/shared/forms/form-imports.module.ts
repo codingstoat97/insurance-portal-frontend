4e53ef8130d58e04521a5822786e22dedebfe499
steffany <steffany.chavez@cirries.com>
2025-11-26 14:13:40 -0400

    Added benefits button on plan form

--- src/app/shared/forms/form-imports.module.ts
+++ src/app/shared/forms/form-imports.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+
 import { SharedModule } from '../shared.module';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,7 +25,9 @@ import { SharedModule } from '../shared.module';
     MatSelectModule,
     MatButtonModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class FormImportsModule { }
