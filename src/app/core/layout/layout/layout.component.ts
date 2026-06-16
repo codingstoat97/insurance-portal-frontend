7a92ab2aa0d75c1845218da2586eff206dfffcab
steffany <steffany.chavez@cirries.com>
2026-06-15 21:49:59 -0400

    layout improvements

--- src/app/core/layout/layout/layout.component.ts
+++ src/app/core/layout/layout/layout.component.ts
@@ -1,33 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
-import { filter } from 'rxjs';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
-import { ToolbarComponent } from "../toolbar/toolbar.component";
-import { FooterComponent } from "../footer/footer.component";
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { ToolbarService } from '../../services/toolbar/toolbar.service';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    ToolbarComponent,
-    FooterComponent],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.sass']
 })
-export class LayoutComponent {
-  hideToolbar = false;
+export class LayoutComponent implements OnDestroy {
+  toolbarVisible = true;
   hideFooter = false;
 
-  constructor(private router: Router) {
-    this.router.events
-      .pipe(filter(e => e instanceof NavigationEnd))
-      .subscribe(() => {
-        const url = this.router.url.split('?')[0];
-        this.hideToolbar = url === '/login' || url.startsWith('/quotes');
-        this.hideFooter = url.startsWith('/quotes');
-      });
+  private subscription = new Subscription();
+
+  constructor(private router: Router, toolbarService: ToolbarService) {
+    this.subscription.add(
+      toolbarService.isVisible$.subscribe(v => (this.toolbarVisible = v))
+    );
+    this.subscription.add(
+      this.router.events
+        .pipe(filter(e => e instanceof NavigationEnd))
+        .subscribe(() => {
+          const url = this.router.url.split('?')[0];
+          this.hideFooter = url.startsWith('/quotes');
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
